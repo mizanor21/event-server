ec486e3f1a32a2b054e329b82efd9c42a1126986
mizan21 <mizanorrahman398@gmail.com>
2024-06-20 16:33:14 +0600

    Updated part added

--- index.js
+++ index.js
@@ -30,12 +30,18 @@ async function run() {
       res.send("Event server is running");
     });
 
-    app.get("/events", (req, res) => {
-      eventCollections
-        .find()
-        .toArray()
-        .then((events) => res.json(events))
-        .catch((error) => console.error(error));
+    app.get("/events", async (req, res) => {
+      const query = {};
+      const cursor = eventCollections.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await eventCollections.findOne(query);
+      res.send(result);
     });
 
     app.post("/events", async (req, res) => {
@@ -48,6 +54,35 @@ async function run() {
         res
           .status(500)
           .json({ error: "An error occurred while adding the event." });
+      }
+    });
+
+    app.put("/event-edit/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedEvent = req.body;
+      try {
+        const query = { _id: new ObjectId(id) };
+        const update = { $set: updatedEvent };
+        const options = { upsert: true };
+        const result = await eventCollections.updateOne(query, update, options);
+
+        if (result.matchedCount === 0 && result.upsertedCount === 1) {
+          res
+            .status(201)
+            .json({
+              message: "Event created successfully",
+              upsertedId: result.upsertedId,
+            });
+        } else if (result.matchedCount > 0) {
+          res.status(200).json({ message: "Event updated successfully" });
+        } else {
+          res.status(404).json({ message: "Event not found" });
+        }
+      } catch (error) {
+        console.error("Error updating event:", error);
+        res
+          .status(500)
+          .json({ error: "An error occurred while updating the event." });
       }
     });
 
